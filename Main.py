6b2c86982f32f9edbed048e3f2c1f8988272dfc5
URK21CS1079 <110010648+URK21CS1079@users.noreply.github.com>
2022-11-06 20:14:21 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -7,16 +7,14 @@ class Evaluate:
   """
     # Write your code here
 
-
   def __init__(self, size):
     """Inits Evaluate with top, size_of_stack and stack.
     Arguments:
       size_of_stack: An integer to set the size of stack.
     """
     self.top = -1
-    self.size_of_stack = size
-    self.stack = []
-
+    self.size = size
+    self.lst = [None]*size
 
   def isEmpty(self):
     """
@@ -24,8 +22,18 @@ class Evaluate:
     Returns:
       True if it is empty, else returns False.
     """
-      # Write your code here
-
+    # Write your code here
+    if self.top == -1:
+       return 1
+    else :
+       return 0
+          
+  def is_full(self):
+    # Write code here
+    if self.top == (self.size - 1):
+      return 1
+    else :
+      return 0
 
   def pop(self):
     """
@@ -34,7 +42,11 @@ class Evaluate:
       The data which is popped out if the stack is not empty.
     """
     # Write your code here
-
+    if not self.isEmpty():
+      t=self.lst[self.top]
+      del self.lst[self.top]
+      self.top-=1
+      return t
 
   def push(self, operand):
     """
@@ -43,35 +55,9 @@ class Evaluate:
       operand: The operand to be pushed.
     """
     # Write your code here
-
+    if not self.is_full():
+            self.top+=1
+            self.lst[self.top]=operand
 
   def validate_postfix_expression(self, expression):
     """
-    Check whether the expression is a valid postfix expression.
-    Arguments:
-      expression: A String which represents the expression to be validated.
-    Returns:
-      True if the expression is valid, else returns False.
-    """
-    # Write your code here
-
-
-  def evaluate_postfix_expression(self, expression):
-    """
-    Evaluate the postfix expression
-    Arguments:
-      expression: A String which represents the the expression to be evaluated
-    Returns:
-      The result of evaluated postfix expression.
-    """
-    # Write your code here
-
-
-# Do not change the following code
-postfix_expression = input()  # Read postfix expression
-tokens = postfix_expression.split()
-evaluate = Evaluate(len(tokens))
-if evaluate.validate_postfix_expression(tokens):
-    print(evaluate.evaluate_postfix_expression(tokens))
-else:
-    print('Invalid postfix expression')
